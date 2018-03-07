7affad3ec85f59d81073e80b2e8cb62a0d7ae610
Colin Smith <cmsgoboston41@gmail.com>
2018-03-07 07:04:43 -0500

    Setup socket.io

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-import app from 'express';
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const port = 3000;
+
+server.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+	console.log('user connected');
+	socket.emit('message', { AdminBot: 'Hey how are you?'});
+	socket.on('another event', (data) => {
+		console.log(data);
+	})
+})
